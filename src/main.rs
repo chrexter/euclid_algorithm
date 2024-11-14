86d3b29f84bdf292db2e4cfdd62030a036976402
Alexander Nitiola <anitiola@aellacredit.com>
2024-11-14 20:42:10 +0100

    observed how this algorithm works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn greatest_common_divisor(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            let t = m;
+
+            m = n;
+            n = t;
+        }
+
+        m = m % n;
+    }
+
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(greatest_common_divisor(14, 15), 1);
+
+    assert_eq!(
+        greatest_common_divisor(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19,),
+        3 * 11
+    );
+}
